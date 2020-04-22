93dd1f4068c0a9d70775c6bbbc5d7282bc81994f
Priscila Da Dalt <priscila.dalt@aluno.faculdadeimpacta.com.br>
2020-04-21 22:33:26 -0300

    Update web.py
    teste pipeline v2

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Olá Priscila-v1"
+    return "Olá Priscila-v2"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
